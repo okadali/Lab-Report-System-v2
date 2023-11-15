e3048c65b5d75e4369c0769b9fa6fb4e18139456
okadali <okadali@hotmail.com>
2023-11-15 17:26:51 +0300

    feat: createmodal init.

--- lab-report-system-v2-frontend/src/components/createModal.js
+++ lab-report-system-v2-frontend/src/components/createModal.js
@@ -1,11 +1,82 @@
-import { Modal } from "reactstrap";
+import { useEffect, useState } from "react";
+import { Button, Col, Input, Modal } from "reactstrap";
 
 const CreateModal = ({ isOpen, toggle }) => {
+
+  const [createdReport,setCreatedReport] = useState({})
+
+  useEffect(() => {
+    setCreatedReport({
+      name:"",
+      surname:"",
+      tc_id:"",
+      title:"",
+      details:""
+    })
+  },[isOpen])
+
+  const onCreateClick = () => {
+    console.log({createdReport});
+    //const response =  _uploadFile()
+    // if(response.status == 200)
+    // const createResponse _createReport(respone.data.data)
+  };
+
+  const INPUT_TYPES = [
+    {
+      id: "name",
+      placeholder: "Patients Name",
+      type: "text",
+    },
+    {
+      id: "surname",
+      placeholder: "Patients Surname",
+      type: "text",
+    },
+    {
+      id: "tc_id",
+      placeholder: "Tc. ID",
+      type: "text",
+    },
+    {
+      id: "title",
+      placeholder: "Title",
+      type: "text",
+    },
+    {
+      id: "details",
+      placeholder: "Details",
+      type: "textarea",
+    },
+  ];
+
+  const onInputChange = (e) => {
+    setCreatedReport({ ...createdReport, [e.target.id]: e.target.value });
+  };
+
   return (
-    <Modal isOpen={isOpen} toggle={toggle} >
-      <h1>haha</h1>
+    <Modal isOpen={isOpen} toggle={toggle}>
+      <div className="p-[15px]">
+        <Col>
+          {INPUT_TYPES.map(({id,placeholder,type}) => {
+            return <Input
+              id={id}
+              placeholder={placeholder}
+              type={type}
+              className="mb-[20px]"
+              onChange={onInputChange}
+            />
+          })}
+          <Input type="file"></Input>
+          <div className="flex justify-end">
+            <Button color="primary" onClick={onCreateClick}>
+              Create
+            </Button>
+          </div>
+        </Col>
+      </div>
     </Modal>
-  )
-}
+  );
+};
 
 export default CreateModal;
--- lab-report-system-v2-frontend/src/pages/report.js
+++ lab-report-system-v2-frontend/src/pages/report.js
@@ -1,18 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useGlobalContext } from "../context/context";
 import SearchBar from "../components/search";
 import Content from "../components/content";
+import { Button } from "reactstrap";
+import CreateModal from "../components/createModal";
 
 const ReportPage = () => {
-  const { auth, setAuth } = useGlobalContext();
 
-  useEffect(() => {
+  const [createModal,setCreateModal] = useState(false);
 
-  }, [auth]);
+  const onCreateButtonClick = () => setCreateModal(true)
 
   return (
     <div>
+      <CreateModal isOpen={createModal} toggle={() => setCreateModal(!createModal)}/>
       <SearchBar/>
+      <Button className="mt-[40px]" color="primary" onClick={onCreateButtonClick}>Create Report</Button>
       <Content/>
     </div>
   );
